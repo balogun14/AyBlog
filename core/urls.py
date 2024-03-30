57039ab704443d24f6e6753306048ca6ad82195e
balogun14 <awwalbalogun06@gmail.com>
2024-03-30 17:44:05 +0100

    Finish Admin Page

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("tinymce",include("tinymce.urls")),
